f72073532113737244400db06ca41674b7b1041f
Lucas G. R <lucky.rodriguesguerra@gmail.com>
2024-06-03 18:13:35 -0300

    s

--- src/routes/ProductRoute.js
+++ src/routes/ProductRoute.js
@@ -3,7 +3,7 @@ import ProductController from '../controllers/ProductController';
 export default (app) => {
   app.get('/product', ProductController.get);
   app.get('/product/:id', ProductController.get);
-  app.post('/product', ProductController.create);
-  app.patch('/product/:id', ProductController.update);
-  app.delete('/product/:id', ProductController.destroy);
+  app.post('/product/persist', ProductController.create);
+  app.patch('/product/persist/:id', ProductController.update);
+  app.delete('/product/delete/:id', ProductController.destroy);
 };
