60a0b2986e7b1b7bf843f54e4d7c2136255f41a2
Lucas <lucas_rodriguesguerra@unochapeco.edu.br>
2024-11-15 22:07:38 -0300

    feat: cagada arrumada

--- src/routes/pedidoServicoRoute.js
+++ src/routes/pedidoServicoRoute.js
@@ -2,8 +2,8 @@ import controller from '../controllers/pedidoServicoController';
 
 export default (app) => {
   app.get('/pedidoServico', controller.get);
-  app.get('/pedidoServico/:cpf', controller.get);
+  app.get('/pedidoServico/:idPedido/:idServico', controller.get);
   app.post('/pedidoServico/create', controller.create);
-  app.patch('/pedidoServico/update/:cpf', controller.update);
-  app.delete('/pedidoServico/delete/:cpf', controller.destroy);
+  app.patch('/pedidoServico/update/:idPedido/:idServico', controller.update);
+  app.delete('/pedidoServico/delete/:idPedido/:idServico', controller.destroy);
 };
